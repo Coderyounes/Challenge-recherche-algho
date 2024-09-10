1fedc354a7ee7e9a05ce72917ebaede7c6334a18
Coderyounes <younesanas2021@gmail.com>
2024-09-10 14:47:50 +0100

    commit until other time in a day

---
+++ rechercheSousChaine.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+    char text[] = "morroco";
+    char search[] = "rr";
+
+    while(text[i] != '\0') {
+        // LOGIC
+        i++;
+    }
+    return (0);
+}
